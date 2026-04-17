a4dc2cac0783f69d7a9dd38e02d02ba316526385
mhay10 <maxwellhay1@gmail.com>
2026-04-17 13:21:24 -0600

    Add bracket match selection. Still no bracket UI

--- project/client/src/routes/manage-tournament/ManageBrackets.svelte
+++ project/client/src/routes/manage-tournament/ManageBrackets.svelte
@@ -2,13 +2,32 @@
     import type {
         TournamentProps,
         Tournament,
+        Bracket,
     } from "$lib/types/manageTournament";
     import { onMount, onDestroy } from "svelte";
+    import { Label, Select } from "flowbite-svelte";
 
     let { tournamentData }: TournamentProps = $props();
 
     let liveTournamentData = $state<Tournament | null>(null);
     let intervalId: any;
+
+    let selectedBracketName = $state<string>("");
+    let selectedBracket = $derived<Bracket | null>(
+        liveTournamentData
+            ? liveTournamentData.brackets.find(
+                  (b) => b.name === selectedBracketName,
+              ) || null
+            : null,
+    );
+    let availableBrackets = $derived(
+        liveTournamentData
+            ? liveTournamentData.brackets.map((b) => ({
+                  value: b.name,
+                  name: b.name,
+              }))
+            : [],
+    );
 
     onMount(() => {
         liveTournamentData = tournamentData.tournament;
@@ -50,8 +69,22 @@
     {/if}
 </div>
 
-<pre>{JSON.stringify(
-        liveTournamentData || tournamentData?.tournament,
-        null,
-        2,
-    )}</pre>
+<section class="w-full max-w-lg mx-auto mb-6">
+    <Label for="bracket-name">Select Bracket</Label>
+    <Select
+        name="bracket-name"
+        items={availableBrackets}
+        bind:value={selectedBracketName}
+        required
+        class="w-full rounded-md border-gray-400"
+    />
+</section>
+
+{#if !!selectedBracket}
+    <h2 class="text-xl font-bold mb-4">
+        Selected Bracket: <i>{selectedBracketName}</i>
+    </h2>
+    <pre>{JSON.stringify(selectedBracket.matches, null, 2)}</pre>
+{:else}
+    <p class="text-gray-500 mt-4">Please select a bracket to view matches.</p>
+{/if}
